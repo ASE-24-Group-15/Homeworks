37a2ee5bd03a5351fb1bfb9a0bf9e71bb240d173
dhirajv2000 <djv2000123@gmail.com>
2024-01-16 00:25:33 -0500

    New test case added for round method

--- test/tests.py
+++ test/tests.py
@@ -1,28 +1,27 @@
-'''
 import sys
 import os
 current_dir = os.path.dirname(os.path.realpath(__file__))
 root_dir = os.path.abspath(os.path.join(current_dir, ".."))
-
 sys.path.insert(1, root_dir)
 
-from src.num import NUM
-from src.sym import SYM
+# from src.num import NUM
+# from src.sym import SYM
+from src.l import l
 
-def test_Num():
-    num = NUM()
-    nums = [5, 5, 5, 5, 10, 10]
-    for n in nums:
-        num.add(n)
-    return 40//6 == num.mid() and 2.581988897471611 == num.div()
+# def test_Num():
+#     num = NUM()
+#     nums = [5, 5, 5, 5, 10, 10]
+#     for n in nums:
+#         num.add(n)
+#     return 40//6 == num.mid() and 2.581988897471611 == num.div()
 
-def test_Sym():
-    sym = SYM()
-    syms = ['s', 's', 'd', 'd', 's', 's']
-    for s in syms:
-        sym.add(s)
-    return 's' == sym.mid and 0.9182958340544896 == sym.div()
-'''
+# def test_Sym():
+#     sym = SYM()
+#     syms = ['s', 's', 'd', 'd', 's', 's']
+#     for s in syms:
+#         sym.add(s)
+#     return 's' == sym.mid and 0.9182958340544896 == sym.div()
+
 import os
 import subprocess
 import sys
@@ -54,6 +53,12 @@ class TestGateScript(unittest.TestCase):
             print(f"Error while running the command: {e}")
             print(f"Command: {' '.join(command)}")
             self.fail("Test failed.")
+    
+    def test_rnd_test(self):
+       self.assertEqual(l().rnd(23.324234), 23.32)
+       self.assertEqual(l().rnd(23.324234, 3), 23.324)
+    
+        
 
 if __name__ == "__main__":
     unittest.main()
