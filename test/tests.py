a22744e2e3a9d82d953b10b5fd115bee39ea5059
Soundarya Khanapur <soundarya.m.khanapur@gmail.com>
2024-01-15 21:06:13 -0500

    initial test cases added

---
+++ test/tests.py
@@ -0,0 +1,20 @@
+import num
+import sym
+
+
+def test_Num():
+    num = num.Num()
+    nums = [5, 5, 5, 5, 10, 10]
+    for n in nums:
+        num.add(n)
+    return 40//6 == num.mid() and 2.581988897471611 == num.div()
+
+def test_Sym():
+    sym = sym.Sym()
+    syms = ['s', 's', 'd', 'd', 's', 's']
+    for s in syms:
+        sym.add(s)
+    return 's' == sym.mid and 0.9182958340544896 == sym.div()
+
+
+    
