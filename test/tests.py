bf17605b326b1d8e574160ef81285ae1602d6cb6
Sharat Neppalli <56881419+Sharatn7@users.noreply.github.com>
2024-01-15 23:49:26 -0500

    Update tests.py

--- test/tests.py
+++ test/tests.py
@@ -5,23 +5,19 @@ root_dir = os.path.abspath(os.path.join(current_dir, ".."))
 
 sys.path.insert(1, root_dir)
 
-from src import num
-from src import sym
-
+from src.num import NUM
+from src.sym import SYM
 
 def test_Num():
-    num = num.Num()
+    num = NUM()
     nums = [5, 5, 5, 5, 10, 10]
     for n in nums:
         num.add(n)
     return 40//6 == num.mid() and 2.581988897471611 == num.div()
 
 def test_Sym():
-    sym = sym.Sym()
+    sym = SYM()
     syms = ['s', 's', 'd', 'd', 's', 's']
     for s in syms:
         sym.add(s)
     return 's' == sym.mid and 0.9182958340544896 == sym.div()
-
-
-    
