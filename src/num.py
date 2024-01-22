38ee9c818ebc22a4fd59ab478494540abc34c1ef
Soundarya Khanapur <soundarya.m.khanapur@gmail.com>
2024-01-22 08:59:06 -0500

    likelihood for num added

--- src/num.py
+++ src/num.py
@@ -27,4 +27,12 @@ class NUM:
     
     def div(self):
         return 0 if self.n < 2 else (self.m2 / self.n - 1) ** 0.5
+
+    #Likelihood
+    def like(self, x):
+        mu, sd = self.mid(), self.div() + 1E-30
+        nom = 2.718 ** (-.5 * (x - mu) ** 2 / (sd ** 2))
+        denom = (sd * 2.5 + 1E-30)
+        return nom / denom
+    
             
