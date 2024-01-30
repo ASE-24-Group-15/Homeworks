f4d4593d76975cf5936a6fe1318e7c67dbf3c5cc
dhirajv2000 <djv2000123@gmail.com>
2024-01-29 19:13:43 -0500

    Modified num.py

--- src/num.py
+++ src/num.py
@@ -8,7 +8,7 @@ class NUM:
         self.m2 = 0
         self.hi = -1E30
         self.lo = 1E30
-        self.heaven = s if (s or " ").endswith("−") else 1
+        self.heaven = 0 if (s or " ").endswith("-") else 1
 
         
     #Update
@@ -34,5 +34,8 @@ class NUM:
         nom = 2.718 ** (-.5 * (x - mu) ** 2 / (sd ** 2))
         denom = (sd * 2.5 + 1E-30)
         return nom / denom
+    
+    def norm(self, x):
+        return x if x == "?" else (x - self.lo) / (self.hi - self.lo + 1E-30)
     
             
