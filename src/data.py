43ac4f8131d432d9ac354e0f91382ee18a3e812c
dhirajv2000 <djv2000123@gmail.com>
2024-01-29 19:12:43 -0500

    Modified data.pu

--- src/data.py
+++ src/data.py
@@ -10,7 +10,7 @@ class DATA:
         if isinstance(src, str):
             for x in csv(src):
                 self.add(x, fun)
-        elif src is not None:
+        else:
             for x in src:
                 self.add(x, fun)
         
@@ -23,6 +23,13 @@ class DATA:
         else:
             self.cols = COLS(row)
     
+    def mid(self, cols=None):
+        u = []
+        for col in cols or self.cols.all.values():
+            u.append(col.mid())
+        return ROW(u)
+
+    
     def stats(self, cols = None, fun = None, ndivs = None):
         u = {".N" : len(self.rows)}
         targetCols = getattr(self.cols, fun or "y")
@@ -30,3 +37,43 @@ class DATA:
             u[targetCols[col].txt] = l().rnd(getattr(targetCols[col], fun or "mid")(), ndivs)
         print(u)
         return u
+    
+    def gate(self, budget0, budget, some):
+        stats, bests = [], []
+        rows = l.shuffle(self.rows)
+        lite = rows[:budget0]
+        dark = rows[budget0:]
+        # print("1. top6", [[row.cells[i] for i in list(self.cols.y.keys())] for row in rows[:6]])
+        # print("1. top50", [[row.cells[i] for i in list(self.cols.y.keys())] for row in rows[:50]])
+        for i in range(budget):
+            best, rest = self.bestRest(lite, len(lite) ** some)
+            todo, selected = self.split(best, rest, lite, dark)
+            stats.append(selected.mid())
+            bests.append(best.rows[0])
+            lite.append(dark.pop(todo))
+        return stats, bests
+            
+            
+    def bestRest(self, rows, want):
+        rows = sorted(rows, key=lambda x: x.d2h(self))
+        best, rest = self.cols.names[:], self.cols.names[:]
+        for i, row in enumerate(rows):
+            if i < want:
+                best.append(row)
+            else:
+                rest.append(row)
+        return DATA(best), DATA(rest)
+    
+    def split(self, best, rest, lite, dark):
+        selected = DATA(self.cols.names)
+        max = 1E30
+        out = 0
+        for i, row in enumerate(dark):
+            b = row.like(best, len(lite), 2)
+            r = row.like(rest, len(lite), 2)
+            if b > r :
+                selected.add(row)
+            tmp = abs(b+r) / abs(b -r + 1E-300)
+            if tmp > max:
+                out, max = i, tmp
+        return out, selected
