c6a69b109aa9c73408f5765db186c42882e3bf7f
Sharat7 <sarathn08@gmail.com>
2024-02-13 14:31:05 -0500

    Delete far.py

--- src/far.py
+++
@@ -1,9 +0,0 @@
-from src.data import DATA
-from src.l import l
-
-def far():
-    d = DATA("../data/auto93.csv")
-    a, b, distance = d.farapart(d)
-    l_instance = l()
-    print(f'far1: {l_instance.o(a)},\nfar2: {l_instance.o(b)}')
-    print(f'distance = {distance}')
