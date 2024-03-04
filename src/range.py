a860b94189a1a82de2843ff095b0545149223248
Soundarya Khanapur <soundarya.m.khanapur@gmail.com>
2024-03-04 12:15:25 -0500

    RANGE class implemented

--- src/range.py
+++ src/range.py
@@ -1,5 +1,47 @@
 import math
 import src.config as config 
+from src.l import l
 
 class RANGE:
-    pass 
+    def __init__(self, at, txt, lo, hi=None):
+        self.at = at
+        self.txt = txt
+        self.x = {"lo": lo, "hi": hi or lo}
+        self.y = {}
+
+    def add(self, x, y):
+        self.x["lo"] = min(self.x["lo"], x)
+        self.x["hi"] = max(self.x["hi"], x)
+        self.y[y] = self.y.get(y, 0) + 1
+
+    def show(self):
+        lo, hi, s = self.x["lo"], self.x["hi"], self.txt
+        if lo == -math.inf:
+            return f"{s} < {hi}"
+        elif hi == math.inf:
+            return f"{s} >= {lo}"
+        elif lo == hi:
+            return f"{s} == {lo}"
+        else:
+            return f"{lo} <= {s} < {hi}"
+        
+    def score(self, goal, LIKE, HATE):
+        return l.score(self.y, goal, LIKE, HATE)
+
+    def merge(self, other):
+        both = RANGE(self.at, self.txt, self.x["lo"])
+        both.x["lo"] = min(self.x["lo"], other.x["lo"])
+        both.x["hi"] = max(self.x["hi"], other.x["hi"])
+        for k, v in self.y.items():
+            both.y[k] = both.y.get(k, 0) + v
+        for k, v in other.y.items():
+            both.y[k] = both.y.get(k, 0) + v
+        return both
+    
+
+    def merged(self, other, tooFew):
+        both = self.merge(other)
+        e1, n1 = l.entropy(self.y)
+        e2, n2 = l.entropy(other.y)
+        if n1 <= tooFew or n2 <= tooFew or l.entropy(both.y) <= (n1 * e1 + n2 * e2) / (n1 + n2):
+            return both
